74803a0adae5d7a98885f0ea63b6cca28c64eba0
Marija Semionova <s047307@ad.viko.lt>
2024-06-18 00:44:14 +0300

    atnaujintas urls

--- stars/urls.py
+++ stars/urls.py
@@ -7,5 +7,5 @@ from stars import views
 app_name = "stars"
 urlpatterns = [
     path('', views.index2, name='index'),
-    # path('star/details/<int:star_id>/', views.star_details, name='star_details'),
+    path('star/details/<int:star_id>/', views.star_details, name='star_details'),
 ]
