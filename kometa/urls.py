a27e9b57839395846e27bee06358ae1431afbc6c
Marija Semionova <s047307@ad.viko.lt>
2024-06-18 13:19:25 +0300

    Atnjaujinti urls ir tests

--- kometa/urls.py
+++ kometa/urls.py
@@ -7,5 +7,5 @@ from kometa import views
 app_name = "comet"
 urlpatterns = [
     path('', views.index2, name='index'),
-    # path('comet/details/<int:comet_id>/', views.comet_details, name='comet_details'),
+    path('comet/details/<int:comet_id>/', views.comet_details, name='comet_details'),
 ]
